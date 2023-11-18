fd6f9f118714fffcdc021b3f057412635adcd752
Mustafa <mustafa.goktas07@gmail.com>
2023-11-18 23:23:06 +0300

    stun servers updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import "./app.scss";
 const ICEServers = {
     iceServers: [
         {
-            urls: ["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"]
+            urls: [
+                "stun:stun.l.google.com:19302", 
+                "stun:stun1.l.google.com:19302", 
+                "stun:stun2.l.google.com:19302", 
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302"
+            ]
         }
     ],
     iceCandidatePoolSize: 10
